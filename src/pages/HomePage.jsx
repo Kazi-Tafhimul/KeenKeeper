2019ffbebd91edb41a5b06f77c364e6f62c067d6
Kazi-Tafhimul <tafhimulkazi@gmail.com>
2026-04-15 22:51:53 +0600

    home page section design half done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { FaPlus } from 'react-icons/fa';
 
 const HomePage = () => {
     return (
         <div>
-            <h2>it is home page</h2>
+            <div className='flex flex-col justify-center items-center mt-20'>
+                <h2 className='font-bold text-4xl'>Friends to keep close in your life</h2>
+                <p className='text-center font-normal text-xl text-gray-400 mt-4'>Your personal shelf of meaningful connections. Browse, tend, and nurture the <br />
+relationships that matter most.</p>
+               <button className='btn bg-green-700 text-white mt-4 border rounded-md hover:bg-green-500'><FaPlus/>Add a friend</button>
+            </div>
             
         </div>
     );
