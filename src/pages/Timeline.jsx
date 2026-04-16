413d4eebe1152b5af2edf3af4de9b38cde192302
Kazi-Tafhimul <tafhimulkazi@gmail.com>
2026-04-17 01:05:13 +0600

    timeline section bug fix

--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { useTimeline } from '../context/useTimeline';
 
 const Timeline = () => {
+    const {timeLine} = useTimeline();
+    if(timeLine.length === 0){
+        return (
+            <div className='font-extrabold text-3xl text-center flex justify-center items-center my-16'>
+                No interaction yet
+            </div>
+        )
+    }
+
     return (
+        
         <div>
-            <h2>it is timeline page</h2>
+            <h1 className='text-xl font-bold'>Timeline</h1>
+            {
+                timeLine.map((entry,index) => (
+                    <div key={index}>
+                        <p>{entry.action} with {entry.friendName}</p>
+                        <p>{entry.date}</p>
+                    </div>
+                ))
+            }
+           
         </div>
     );
 };
