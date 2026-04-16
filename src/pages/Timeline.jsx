a92cde10589865fd3e030902dd0fb7291afbe8dc
Kazi-Tafhimul <tafhimulkazi@gmail.com>
2026-04-17 01:21:24 +0600

    timeline card fix

--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { useTimeline } from '../context/useTimeline';
+import callImg from '../assets/call.png';
+import textImg from '../assets/text.png';
+import videoImg from '../assets/video.png';
+
 
 const Timeline = () => {
     const {timeLine} = useTimeline();
+    const actionImages = {
+        "Call":callImg,
+        "Text":textImg,
+        "Video":videoImg
+    }
     if(timeLine.length === 0){
         return (
             <div className='font-extrabold text-3xl text-center flex justify-center items-center my-16'>
@@ -14,12 +23,17 @@ const Timeline = () => {
     return (
         
         <div>
-            <h1 className='text-xl font-bold'>Timeline</h1>
+            <h1 className='text-xl font-bold mb-6'>Timeline</h1>
             {
                 timeLine.map((entry,index) => (
-                    <div key={index}>
-                        <p>{entry.action} with {entry.friendName}</p>
-                        <p>{entry.date}</p>
+                    <div key={index} className='flex flex-col shadow-sm space-y-3 p-5'>
+                        <div className='flex items-center gap-4'>
+                        <img src = {actionImages[entry.action]} className='w-5 h-5 object-contain' alt="" />
+
+                        <p className='font-semibold text-2xl'>{entry.action} <span className='font-normal text-xl text-gray-500'>with {entry.friendName}</span> </p>
+                        </div>
+
+                        <p className='font-normal text-sm'>{entry.date}</p>
                     </div>
                 ))
             }
