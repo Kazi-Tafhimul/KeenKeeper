4002e2f4aca44d64b40d76c585743989a3003ee9
Kazi-Tafhimul <tafhimulkazi@gmail.com>
2026-04-17 15:45:24 +0600

    filtering logic added in timeline page

--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTimeline } from '../context/useTimeline';
 import callImg from '../assets/call.png';
 import textImg from '../assets/text.png';
@@ -7,11 +7,18 @@ import videoImg from '../assets/video.png';
 
 const Timeline = () => {
     const {timeLine} = useTimeline();
+    const [filter, setFilter] = useState("All");
     const actionImages = {
         "Call":callImg,
         "Text":textImg,
         "Video":videoImg
     }
+    const handleFilter = (type) =>{
+        setFilter(type);
+
+    }
+    const filteredTimeline = filter === "All"? timeLine 
+    : timeLine.filter(entry => entry.action === filter);
     if(timeLine.length === 0){
         return (
            <div className='flex justify-center items-center min-h-screen'>
@@ -22,22 +29,42 @@ const Timeline = () => {
         )
     }
 
+
     return (
         
         <div>
-            <h1 className='text-xl font-bold mb-6'>Timeline</h1>
+            <h1 className='text-xl font-bold mb-4 p-4'>Timeline</h1>
+            <details className="dropdown ">
+                <summary className="btn m-1">Filter Timeline : {filter}</summary>
+                <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                    <li><a onClick={()=> handleFilter("All")}>All</a></li>
+                    <li><a onClick={() => handleFilter("Call")}>Call</a></li>
+                    <li><a onClick={()=> handleFilter("Text")}>Text</a></li>
+                    <li><a onClick={() => handleFilter("Video")}>Video</a></li>
+                </ul>
+             </details>
             {
-                timeLine.map((entry,index) => (
-                    <div key={index} className='flex flex-col shadow-sm space-y-3 p-5'>
-                        <div className='flex items-center gap-4'>
-                        <img src = {actionImages[entry.action]} className='w-5 h-5 object-contain' alt="" />
+                filteredTimeline.length>0?(
 
-                        <p className='font-semibold text-2xl'>{entry.action} <span className='font-normal text-xl text-gray-500'>with {entry.friendName}</span> </p>
+                    filteredTimeline.map((entry,index) => (
+                        <div key={index} className='flex flex-col shadow-sm space-y-3 p-5'>
+                            <div className='flex items-center gap-4'>
+                            <img src = {actionImages[entry.action]} className='w-5 h-5 object-contain' alt="" />
+    
+                            <p className='font-semibold text-2xl'>{entry.action} <span className='font-normal text-xl text-gray-500'>with {entry.friendName}</span> </p>
+                            </div>
+    
+                            <p className='font-normal text-sm'>{entry.date}</p>
                         </div>
+                    ))
+                ) : (
+                    <div className='flex justify-center items-center min-h-screen'>
+                <h1 className='text-3xl font-bold'>
+                    No interactions to show yet
+                </h1>
+            </div>
 
-                        <p className='font-normal text-sm'>{entry.date}</p>
-                    </div>
-                ))
+                )
             }
            
         </div>
